53fd4d510006aa9b4094fd0ba8c35956dd24d548
Kaushik Subbaratnam <kaushik.sr@gmail.com>
2026-04-02 20:25:55 +0200

    AI prompt changes, AI Summary files, dbt initial work

---
+++ sentinel_dbt/models/silver/knmi_silver_validated.sql
@@ -0,0 +1,12 @@
+select
+    observed_at,
+    location,
+    data_provider,
+    temp,
+    humidity,
+    wind_speed,
+    is_valid,
+    dq_flag,
+    processed_at
+from {{ source('silver', 'weather_silver') }}
+where data_provider = 'knmi'
